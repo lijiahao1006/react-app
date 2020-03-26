3b916f18afa59ea194a030f41843bbfa99616201
Pluslee <lijiahao1006@yandex.com>
2020-03-26 12:11:44 +0800

    reducer/switch-case

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,17 +1,26 @@
 const initialState = {
-  'a' : 999,
-  'b': 5
-}
+  a: 999,
+  b: 5
+};
 
-export default (state = initialState, {type}) => {
-    if (type === 'add') {
-      return {
-        'a': state.a+1
-      }
-    }else if (type === 'cheng') {
-      return {
-        'b': state.b*2
-      }
-    }
-    return state;
-}
+export default (state = initialState, { type }) => {
+  // if (type === 'add') {
+  //   return {
+  //     'a': state.a+1
+  //   }
+  // }else if (type === 'cheng') {
+  //   return {
+  //     'b': state.b*2
+  //   }
+  // }
+  // return state;
+  switch (type) {
+    case "add":
+      return { a: state.a + 2 };
+    case "cheng":
+      return { b: state.b * 10 };
+
+    default:
+      return state;
+  }
+};
